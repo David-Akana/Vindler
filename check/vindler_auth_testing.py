247dec6d5ac48ad0ede6315db0db2d4a889dad73
David-Akana <56788648+David-Akana@users.noreply.github.com>
2022-03-07 00:38:20 +0100

    updated testing script

--- check/vindler_auth_testing.py
+++ check/vindler_auth_testing.py
@@ -5,9 +5,9 @@ import os
 # for single files 
 '''
 url = 'http://127.0.0.1:8000/facepredict/'
-#directory = './test_male'
+#directory = './test/3/opencv_frame_david_6.png'
 #directory = './test_female'
-files = { "images":open('./test_male/sweetboy0.jpg', 'rb') }
+files = { "images":open('./test/3/opencv_frame_david_6.png', 'rb') }
 
 print(files)
 requests.post(url, files=files )                                               
@@ -21,15 +21,16 @@ requests.post(url, files=files )
 
 url = 'http://127.0.0.1:8000/faceauth/'
 data = {}
-data["targets"] = "m1a2l3e"
-directory = './train_male'
+#data["targets"] = "m1a2l3e"
+directory = './training/3'
 #directory = './train_female'
 #directory = './see'
 #data["targets"] = "f1e2l3m4l5e"
 
 files=[eval(f'("train_images", open("{directory + "/" + image}", "rb"))') for image in os.listdir(directory) if image != 'Thumbs.db']
 print(files)
-requests.post(url, files=files, data=data )
+#requests.post(url, files=files, data=data )
+requests.post(url, files=files)
 
 
 
